96a50f0ead405bf36f3b4b1000ee4e48dd566503
Xurui Yan <yanxurui1993@qq.com>
2025-07-19 09:43:44 -0700

    Update

--- ddns.py
+++ ddns.py
@@ -68,6 +68,7 @@ def get_ipv6():
 
 
 if __name__ == '__main__':
+    logging.info(f"starting ddns.py, pid: {os.getpid()}")
     try:
         logger.setup_logging()
         conf = json.load(open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "conf.json"), "r"))
--- ip/base.py
+++ ip/base.py
@@ -20,8 +20,9 @@ class IP(object):
             try:
                 ip = sub.get_ip()
                 if ip:
+                    ip = ip.strip()
                     logging.info(f"{sub.__name__} found ip {ip}")
-                    ipDict[ip.strip()] += 1
+                    ipDict[ip] += 1
             except Exception as e:
                 logging.exception(e)
 
